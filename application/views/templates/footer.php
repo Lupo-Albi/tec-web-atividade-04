31542904e3520181af5d92b411e7aa38b6c11cc7
Lupo-Albi <luandersonalbuquerque@gmail.com>
2018-11-01 09:57:35 -0300

    Modificações no template da página e no conteúdo da página home

--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -1,5 +1,14 @@
-    <!-- Scripts JavaScript -->
-    <script src="<?=base_url('static/js/jquery-3.3.1.slim.min.js')?>"></script>
+    <!-- FOOTER -->
+      <footer class="container">
+        <p class="float-right"><a href="#">Back to top</a></p>
+        <!--<p>&copy; 2017-2018 Company, Inc. &middot; <a href="#">Privacy</a> &middot; <a href="#">Terms</a></p>-->
+      </footer>
+    </main>
+	
+	<!-- Principal JavaScript do Bootstrap
+    ================================================== -->
+    <!-- Foi colocado no final para a página carregar mais rápido -->
+    <script src="<?=base_url('static/js/jquery-3.3.1.slim.min.js')?>" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="<?=base_url('static/js/popper.min.js')?>"></script>
     <script src="<?=base_url('static/js/bootstrap.min.js')?>"></script>
   </body>
--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -1,24 +1,40 @@
-<!DOCTYPE html>
+<!doctype html>
 <html lang="pt-br">
-    <head>
-        <meta charset="utf-8">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="../../../../favicon.ico">
 
-        <title>Título para Mudar</title>
+    <title>Template inicial, usando Bootstrap.</title>
 
-        <!-- Bootstrap -->
-        <link href="<?=base_url('static/css/bootstrap.min.css')?>" rel="stylesheet">
-    </head>
+    <!-- Principal CSS do Bootstrap -->
+    <link href="<?=base_url('static/css/bootstrap.min.css')?>" rel="stylesheet">
 
-    <body>
-        <!-- Barra de navegação -->
-        <nav class="navbar navbar-expand-sm bg-primary navbar-dark">
-            <a class="navbar-brand" href="#">Navbar</a>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-        </nav>
+    <!-- Estilos customizados para esse template -->
+    <link href="<?=base_url('static/css/starter-template.css')?>" rel="stylesheet">
+  </head>
 
-       
-
-
-<!-- As tags html e body são fechadas em footer.php -->
+  <body>
+    <header>
+      <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
+        <a class="navbar-brand" href="#">Sistema Hospitalar</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarCollapse">
+          <ul class="navbar-nav mr-auto">
+            <li class="nav-item active">
+              <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Equipe</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link disabled" href="#">Contato</a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </header>
