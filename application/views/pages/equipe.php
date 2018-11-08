3f501cc98c5238ab7f41c2afc7232fae8169a10b
Lupo-Albi <luandersonalbuquerque@gmail.com>
2018-11-08 19:15:04 -0300

    Ajeitando a identação

--- application/views/pages/equipe.php
+++ application/views/pages/equipe.php
@@ -1,23 +1,25 @@
-<div class="container marketing">
-    <br><br><br>
-    <!-- 3 colunas com imagens e texto -->
-    <div class="row">
-    <!--
-        Criando uma iteração em php para exibir as informações dos membros a partir do banco de dados
-        A função Img criada em membros_model é usada nessa iteração para buscar as strings que correspondem as imagens de cada membro na pasta static/img
-        Os nomes das imagens foram nomeados de acordo com o ID de cada membro na sua respectiva tabela do banco de dados
-        e.g.: se um membro tem seu id = 0 no banco, o nome da sua imagem correspondente terá o nome 0.jpg
-    -->
-    <?php foreach ($membros as $membro => $value) { ?>
-        <div class="col-lg-4">
-            <?php $index = $value['id']; ?>
-            <img class="rounded-circle" src="<?=$this->membros_model->Img($index)?>" alt="" width="140" height="140">
-            <h2><?php echo $value['nome']; ?></h2>
-            <p><?php echo $value['bio']; ?></p>
-            <strong>Nome Completo:</strong> <?php echo $value['nome'] . ' ' . $value['sobrenome']; ?><br>
-            <strong>E-mail: </strong><?php echo $value['email']; ?><br>
-            <strong>Telefone: </strong><?php echo $value['telefone']; ?><br>
+<main role='main'>
+    <div class="container marketing">
+        <br><br><br>
+        <!-- 3 colunas com imagens e texto -->
+        <div class="row">
+        <!--
+            Criando uma iteração em php para exibir as informações dos membros a partir do banco de dados
+            A função Img criada em membros_model é usada nessa iteração para buscar as strings que correspondem as imagens de cada membro na pasta static/img
+            Os nomes das imagens foram nomeados de acordo com o ID de cada membro na sua respectiva tabela do banco de dados
+            e.g.: se um membro tem seu id = 0 no banco, o nome da sua imagem correspondente terá o nome 0.jpg
+        -->
+        <?php foreach ($membros as $membro => $value) { ?>
+            <div class="col-lg-4">
+                <?php $index = $value['id']; ?>
+                <img class="rounded-circle" src="<?=$this->membros_model->Img($index)?>" alt="" width="140" height="140">
+                <h2><?php echo $value['nome']; ?></h2>
+                <p><?php echo $value['bio']; ?></p>
+                <strong>Nome Completo:</strong> <?php echo $value['nome'] . ' ' . $value['sobrenome']; ?><br>
+                <strong>E-mail: </strong><?php echo $value['email']; ?><br>
+                <strong>Telefone: </strong><?php echo $value['telefone']; ?><br>
+            </div>
+        <?php } ?>
         </div>
-    <?php } ?>
     </div>
-</div>
+</main>
--- application/views/pages/home.php
+++ application/views/pages/home.php
@@ -1,36 +1,37 @@
-    <main role="main">
-      <!--Carousel Slide-->
-      <div id="myCarousel" class="carousel slide" data-ride="carousel">
-        <ol class="carousel-indicators">
-          <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
-          <li data-target="#myCarousel" data-slide-to="1"></li>
-        </ol>
-        <div class="carousel-inner">
-          <div class="carousel-item active">
-            <img class="d-block w-100" src="<?=base_url('static/img/slide4.jpg')?>" alt="Primeira Imagem">
-            <div class="container">
-              <div class="carousel-caption text-left">
-                <h1 style="color:black;">Integração de Setores.</h1>
-                <p style="color:black;">O intuito é de integrar os diversos setores do estabelecimento, tais como: recepção, consultório, farmácia e administração.</p>
-              </div>
-            </div>
-          </div>
-          <div class="carousel-item">
-            <img class="d-block w-100" src="<?=base_url('static/img/slide2.jpg')?>" alt="Segunda Imagem">
-            <div class="container">
-              <div class="carousel-caption text-right">
-                <h1 style="color:black;">Mais rapidez nos serviços.</h1>
-                <p style="color:black;">Possibilidade de agendamento de consultas médicas e pesquisa de medicamentos.</p>
-              </div>
-            </div>
+<main role="main">
+  <!--Carousel Slide-->
+  <div id="myCarousel" class="carousel slide" data-ride="carousel">
+    <ol class="carousel-indicators">
+      <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+      <li data-target="#myCarousel" data-slide-to="1"></li>
+    </ol>
+    <div class="carousel-inner">
+      <div class="carousel-item active">
+        <img class="d-block w-100" src="<?=base_url('static/img/slide4.jpg')?>" alt="Primeira Imagem">
+        <div class="container">
+          <div class="carousel-caption text-left">
+            <h1 style="color:black;">Integração de Setores.</h1>
+            <p style="color:black;">O intuito é de integrar os diversos setores do estabelecimento, tais como: recepção, consultório, farmácia e administração.</p>
           </div>
         </div>
-        <a class="carousel-control-prev" href="#myCarousel" role="button" data-slide="prev">
-          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-          <span class="sr-only">Previous</span>
-        </a>
-        <a class="carousel-control-next" href="#myCarousel" role="button" data-slide="next">
-          <span class="carousel-control-next-icon" aria-hidden="true"></span>
-          <span class="sr-only">Next</span>
-        </a>
       </div>
+      <div class="carousel-item">
+        <img class="d-block w-100" src="<?=base_url('static/img/slide2.jpg')?>" alt="Segunda Imagem">
+        <div class="container">
+          <div class="carousel-caption text-right">
+            <h1 style="color:black;">Mais rapidez nos serviços.</h1>
+            <p style="color:black;">Possibilidade de agendamento de consultas médicas e pesquisa de medicamentos.</p>
+          </div>
+        </div>
+      </div>
+    </div>
+    <a class="carousel-control-prev" href="#myCarousel" role="button" data-slide="prev">
+      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+      <span class="sr-only">Previous</span>
+    </a>
+    <a class="carousel-control-next" href="#myCarousel" role="button" data-slide="next">
+      <span class="carousel-control-next-icon" aria-hidden="true"></span>
+      <span class="sr-only">Next</span>
+    </a>
+  </div>
+</main>
