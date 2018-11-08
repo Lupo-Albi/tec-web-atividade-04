a724f228f8d185e4c26239f5cd7b1a141eb2a3f4
Lupo-Albi <luandersonalbuquerque@gmail.com>
2018-11-08 15:13:57 -0300

    Ajuste no script SQL

--- SQL Script/tecweb.sql
+++ SQL Script/tecweb.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: 08-Nov-2018 às 12:54
+-- Generation Time: 08-Nov-2018 às 19:05
 -- Versão do servidor: 10.1.36-MariaDB
 -- versão do PHP: 7.2.11
 
@@ -21,6 +21,8 @@ SET time_zone = "+00:00";
 --
 -- Database: `tecweb`
 --
+CREATE DATABASE IF NOT EXISTS `tecweb` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
+USE `tecweb`;
 
 -- --------------------------------------------------------
 
@@ -28,10 +30,12 @@ SET time_zone = "+00:00";
 -- Estrutura da tabela `contatos`
 --
 
-CREATE TABLE `contatos` (
-  `id` int(10) UNSIGNED NOT NULL,
+DROP TABLE IF EXISTS `contatos`;
+CREATE TABLE IF NOT EXISTS `contatos` (
+  `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT,
   `nome` varchar(50) NOT NULL,
-  `email` varchar(50) NOT NULL
+  `email` varchar(50) NOT NULL,
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -40,15 +44,17 @@ CREATE TABLE `contatos` (
 -- Estrutura da tabela `membros`
 --
 
-CREATE TABLE `membros` (
-  `id` int(10) UNSIGNED NOT NULL,
+DROP TABLE IF EXISTS `membros`;
+CREATE TABLE IF NOT EXISTS `membros` (
+  `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT,
   `nome` varchar(20) NOT NULL,
   `sobrenome` varchar(50) NOT NULL,
   `email` varchar(50) NOT NULL,
   `telefone` varchar(15) DEFAULT NULL,
   `datanascimento` date DEFAULT NULL,
-  `bio` varchar(1000) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `bio` varchar(1000) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=latin1;
 
 --
 -- Extraindo dados da tabela `membros`
@@ -56,10 +62,11 @@ CREATE TABLE `membros` (
 
 INSERT INTO `membros` (`id`, `nome`, `sobrenome`, `email`, `telefone`, `datanascimento`, `bio`) VALUES
 (1, 'Luanderson', 'Martins de Albuquerque', 'luanderson-albuquerque@hotmail.com', '88996354532', '1997-05-07', 'é estudante de Engenharia de Computação na Universidade Federal do Ceará - Campus Sobral. Tem afinidade com programação e quer seguir pela vertente de Ciências da Computação no curso. Tem bastante interesse na área de desenvolvimento de jogos, por isso escolheu seguir essa carreira. Gosta de jogar video game no tempo livre.'),
-(2, 'Júlio', 'César Rodrigues de Oliveira', 'jc_96@hotmail.com', '88996662256', '1996-01-05', NULL),
+(2, 'Júlio', 'César Rodrigues de Oliveira', 'jc_96@hotmail.com', '88996662256', '1996-01-05', 'é estudante de engenharia na Universidade Federal do Ceará, Campus Sobral. Dentre as vertentes tem afinidade com a área de telecomunicações. Formado pela EEEP Isaias Gonçalves Damasceno de São Benedito-CE como técnico em Redes de Computadores.'),
 (3, 'Igor', 'Linhares', 'frigfeli@gmail.com', '8899936384', '1997-09-08', NULL),
-(4, 'Amanda', 'Lima Sousa', 'just.amaanda@gmail.com', '994602740', '1994-02-21', NULL),
-(5, 'Ricardo', 'de Souza Silva', 'ricardosouza@alu.ufc.br', '88997354640', '1996-12-07', 'é estudante de Engenharia de Computação desde o primeiro semestre de 2015 na Universidade Federal do Ceará – Campus Sobral, dentre as vertentes do curso em questão, tem uma certa afinidade com Ciências da Computação, área escolhida para atuação, inclusive. Até o momento não apresenta qualquer experiência na área profissional, resumindo-se apenas à vida acadêmica.</p>\r\n<p>Dentre as linguagens da área, tem mais afinidade/habilidade com MATLAB, além de ter preferência pelo estudo relacionado à Inteligência Computacional, principalmente no ramo denominado Morfologia Matemática. Embora também tenha interesse pela área de Tecnologias Web.');
+(4, 'Amanda', 'Lima Sousa', 'just.amaanda@gmail.com', '994602740', '1994-02-21', 'é estudante de Engenharia de Computação, viciada em filmes e aspirante em fotografia e design.'),
+(5, 'Ricardo', 'de Souza Silva', 'ricardosouza@alu.ufc.br', '88997354640', '1996-12-07', 'é estudante de Engenharia de Computação desde o primeiro semestre de 2015 na Universidade Federal do Ceará – Campus Sobral, dentre as vertentes do curso em questão, tem uma certa afinidade com Ciências da Computação, área escolhida para atuação, inclusive. Até o momento não apresenta qualquer experiência na área profissional, resumindo-se apenas à vida acadêmica.</p>\r\n<p>Dentre as linguagens da área, tem mais afinidade/habilidade com MATLAB, além de ter preferência pelo estudo relacionado à Inteligência Computacional, principalmente no ramo denominado Morfologia Matemática. Embora também tenha interesse pela área de Tecnologias Web.'),
+(6, 'Rayon', 'Lindraz Nunes', 'rayonnunes@hotmail.com', '85997488646', '1995-05-15', 'é estudante da Engenharia da Computação - Semestre 2015.1');
 
 -- --------------------------------------------------------
 
@@ -67,9 +74,12 @@ INSERT INTO `membros` (`id`, `nome`, `sobrenome`, `email`, `telefone`, `datanasc
 -- Estrutura da tabela `membro_has_mensagem`
 --
 
-CREATE TABLE `membro_has_mensagem` (
+DROP TABLE IF EXISTS `membro_has_mensagem`;
+CREATE TABLE IF NOT EXISTS `membro_has_mensagem` (
   `fk_idMembro` int(10) UNSIGNED NOT NULL,
-  `fk_idMensagem` int(10) UNSIGNED NOT NULL
+  `fk_idMensagem` int(10) UNSIGNED NOT NULL,
+  KEY `fk_idMensagem` (`fk_idMensagem`),
+  KEY `fk_idMembro` (`fk_idMembro`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -78,11 +88,14 @@ CREATE TABLE `membro_has_mensagem` (
 -- Estrutura da tabela `mensagem`
 --
 
-CREATE TABLE `mensagem` (
-  `id` int(10) UNSIGNED NOT NULL,
+DROP TABLE IF EXISTS `mensagem`;
+CREATE TABLE IF NOT EXISTS `mensagem` (
+  `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT,
   `conteudo` varchar(1000) NOT NULL,
   `data` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `fk_idContato` int(10) UNSIGNED NOT NULL
+  `fk_idContato` int(10) UNSIGNED NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_idContato` (`fk_idContato`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -91,7 +104,8 @@ CREATE TABLE `mensagem` (
 -- Stand-in structure for view `vermensagens`
 -- (See below for the actual view)
 --
-CREATE TABLE `vermensagens` (
+DROP VIEW IF EXISTS `vermensagens`;
+CREATE TABLE IF NOT EXISTS `vermensagens` (
 `Recebido em` timestamp
 ,`Remetente` varchar(50)
 ,`E-mail` varchar(50)
@@ -107,58 +121,6 @@ CREATE TABLE `vermensagens` (
 DROP TABLE IF EXISTS `vermensagens`;
 
 CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW `vermensagens`  AS  select `mensagem`.`data` AS `Recebido em`,`contatos`.`nome` AS `Remetente`,`contatos`.`email` AS `E-mail`,`membros`.`nome` AS `Destinatário`,`mensagem`.`conteudo` AS `Mensagem` from (((`contatos` join `mensagem` on((`contatos`.`id` = `mensagem`.`fk_idContato`))) join `membro_has_mensagem` on((`mensagem`.`id` = `membro_has_mensagem`.`fk_idMensagem`))) join `membros` on((`membro_has_mensagem`.`fk_idMembro` = `membros`.`id`))) ;
-
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `contatos`
---
-ALTER TABLE `contatos`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `membros`
---
-ALTER TABLE `membros`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `membro_has_mensagem`
---
-ALTER TABLE `membro_has_mensagem`
-  ADD KEY `fk_idMensagem` (`fk_idMensagem`),
-  ADD KEY `fk_idMembro` (`fk_idMembro`);
-
---
--- Indexes for table `mensagem`
---
-ALTER TABLE `mensagem`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `fk_idContato` (`fk_idContato`);
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `contatos`
---
-ALTER TABLE `contatos`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
-
---
--- AUTO_INCREMENT for table `membros`
---
-ALTER TABLE `membros`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
-
---
--- AUTO_INCREMENT for table `mensagem`
---
-ALTER TABLE `mensagem`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
 
 --
 -- Constraints for dumped tables
